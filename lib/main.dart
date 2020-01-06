0e1ebd42412837cb5d0264aef62503a4a59806e6
andreyguerra <andrey.guerra@conquistaformaturas.com.br>
2020-01-06 11:10:33 -0300

    Create Project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:loja_teste_2/screens/home_screen.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: "Loja do Capiroto",
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Color.fromARGB(255, 4, 125, 141)
+      ),
+      debugShowCheckedModeBanner: false,
+      home:  HomeScreen(),
+    );
+  }
+}
+
