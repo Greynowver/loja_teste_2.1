2600c142f518e4eec0b69c10d7be3b2401286aff
andreyguerra <andrey.guerra@conquistaformaturas.com.br>
2020-01-07 14:36:21 -0300

    Classe CartModel criada

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loja_teste_2/datas/cart_product.dart';
+import 'package:loja_teste_2/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class CartModel extends Model {
+
+  UserModel user;
+
+  List<CartProduct> products =[];
+
+  CartModel(this.user);
+
+  void addCartItem(CartProduct cartProduct){
+    products.add(cartProduct);
+
+    Firestore.instance.collection("users").document(user.firebaseUser.uid)
+      .collection("cart").add(cartProduct.toMap()).then((doc){
+        cartProduct.cid = doc.documentID;
+    });
+
+    notifyListeners();
+  }
+
+  void removedCartItem(CartProduct cartProduct){
+    Firestore.instance.collection("users").document(user.firebaseUser.uid)
+        .collection("cart").document(cartProduct.cid).delete();
+
+    products.remove(cartProduct);
+
+    notifyListeners();
+  }
+
+}
