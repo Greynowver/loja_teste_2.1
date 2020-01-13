4ae8f6932db86d305bf4078fac399758d08203b6
Greynowver <greynowver@gmail.com>
2020-01-13 17:26:15 -0300

    Card da Loja Criado

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_teste_2/tabs/home_tab.dart';
 import 'package:loja_teste_2/tabs/orders_tab.dart';
+import 'package:loja_teste_2/tabs/places_tab.dart';
 import 'package:loja_teste_2/tabs/products_tab.dart';
 import 'package:loja_teste_2/widgets/cart_buttom.dart';
 import 'package:loja_teste_2/widgets/custom_drawer.dart';
@@ -29,7 +30,14 @@ class HomeScreen extends StatelessWidget {
           body: ProductsTab(),
           floatingActionButton: CartButton(),
         ),
-        Container(color: Colors.yellow,),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Lojas"),
+            centerTitle: true,
+          ),
+          body: PlacesTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
         Scaffold(
           appBar: AppBar(
             title: Text("Meus Pedidos"),
